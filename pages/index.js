a5a99d725c980a92ab817c3416ba05c8fcd7581d
Suraj-bhan <2018mmb1296@iitrpr.ac.in>
2021-08-31 00:25:21 +0530

    added header to the project

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
+import Header from '../Components/Header'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Airbnb-Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hi there </h1>
-    </div>
+      {/* Header */}
+      <Header/>
+      {/* Banner */}
+      
+           </div>
   )
 }
