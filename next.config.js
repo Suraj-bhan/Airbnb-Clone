001b8f8b2163f134cd826d69b2de212abe5804e8
Suraj-bhan <2018mmb1296@iitrpr.ac.in>
2021-10-24 02:02:58 +0530

    added maps component with marker pins

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
         domains: ['links.papareact.com'],
+    },
+    env:{
+        mapbox_key:'pk.eyJ1IjoibWV0YXJ0aXN0IiwiYSI6ImNrdjQ3MGNjMTBjM3MybnF2dmR6Nm1rMjgifQ.hGVqGM1shK5na8U1-FV7VQ'
     }
 }
